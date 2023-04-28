32b7fd585b6861714898f0cf406fc2a7d03c90c2
BaptisteUrgell <baptiste.u@gmail.com>
2023-04-29 00:29:19 +0200

    Modification of CombinedLoss parameters.

--- src/models/losses.py
+++ src/models/losses.py
@@ -2,19 +2,27 @@ import torch.nn as nn
 from torchgeometry.losses.dice import DiceLoss
 
 class CombinedLoss(nn.Module):
-    def __init__(self, bce_weight, dice_weight):
+    def __init__(self, bce_weight=None, dice_weight=1):
         super(CombinedLoss, self).__init__()
-        self.alpha = bce_weight
-        self.beta = dice_weight
+
+        if not ((dice_weight is None) ^ (bce_weight is None)):
+            raise TypeError(f'dice_weight and bce_weight are both None or both defined. dice_weight={type(dice_weight)}, bce_weight={type(bce_weight)}')
+        if not dice_weight is None:
+            self.dice_weight = dice_weight
+            self.bce_weight = 1 - dice_weight
+        else:
+            self.bce_weight = bce_weight
+            self.dice_weight = 1 - bce_weight
+            
         self.bce_loss = nn.BCELoss()
         self.dice_loss = DiceLoss()
         
-    def forward(self, input, target):
+    def forward(self, inputs, targets):
         # Compute binary cross-entropy (BCE) loss
-        bce_loss = self.bce_loss(input, target)
+        bce_loss = self.bce_loss(inputs, targets)
         
         # Compute Dice loss
-        dice_loss = self.dice_loss(input, target)
+        dice_loss = self.dice_loss(inputs, targets)
         
         # Combine the losses using weighted sum
         combined_loss = self.alpha * bce_loss + self.beta * dice_loss
