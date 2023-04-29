35eee94816342b45935b3ffc2ac534611b27245f
BaptisteUrgell <baptiste.u@gmail.com>
2023-04-29 22:58:03 +0200

    reduce Train Fragments list to fit into Kaggle.

--- constant.py
+++ constant.py
@@ -2,7 +2,7 @@ import torch
 
 FRAGMENTS_PATH = 'data/raw/train'
 KAGGLE_FRAGMENTS_PATH = '../../input/vesuvius-challenge-ink-detection/train'
-TRAIN_FRAGMENTS = ['1', '2']
+TRAIN_FRAGMENTS = ['1']
 VAL_FRAGMENTS = ['3']
 
 Z_START = 27
