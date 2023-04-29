7b093e2e8ed515e61f97f4ad45ac4141618fe188
BaptisteUrgell <baptiste.u@gmail.com>
2023-04-29 21:05:08 +0200

    Z_DIM set to 8. Add DEVICE.

--- constant.py
+++ constant.py
@@ -1,7 +1,11 @@
-FRAGMENTS_PATH = '../../data/raw/train'
+import torch
+
+FRAGMENTS_PATH = 'data/raw/train'
 TRAIN_FRAGMENTS = ['1', '2']
 VAL_FRAGMENTS = ['3']
 
 Z_START = 27
-Z_DIM = 10
+Z_DIM = 8
 TILE_SIZE = 256
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
