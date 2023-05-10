d0595bb9bb1fe77334646a992e20bfe5dc585a31
Louis REBERGA <louis.reberga@gmail.com>
2023-05-10 20:07:34 +0200

    load data in GPU progressively + fragment id and center

--- constant.py
+++ constant.py
@@ -3,7 +3,10 @@ from os import pardir
 
 KAGGLE_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'train')
 COOKIECUTTER_FRAGMENTS_PATH = join('data', 'raw', 'train')
-FRAGMENTS_PATH =  KAGGLE_FRAGMENTS_PATH if exists(KAGGLE_FRAGMENTS_PATH) else COOKIECUTTER_FRAGMENTS_PATH
+PARENT_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'train')
+FRAGMENTS_PATH = KAGGLE_FRAGMENTS_PATH if exists(KAGGLE_FRAGMENTS_PATH) \
+                                       else COOKIECUTTER_FRAGMENTS_PATH if exists(COOKIECUTTER_FRAGMENTS_PATH) \
+                                       else PARENT_FRAGMENTS_PATH
 
 KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
 COOKIECUTTER_MODELS_DIR = 'models'
@@ -11,13 +14,13 @@ MODELS_DIR = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_
 
 KAGGLE_TEST_FRAGMENTS = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'test')
 COOKIECUTTER_TEST_FRAGMENTS = join('data', 'raw', 'test')
-TEST_FRAGMENTS =  KAGGLE_TEST_FRAGMENTS if exists(KAGGLE_TEST_FRAGMENTS) else COOKIECUTTER_TEST_FRAGMENTS
+TEST_FRAGMENTS = KAGGLE_TEST_FRAGMENTS if exists(KAGGLE_TEST_FRAGMENTS) else COOKIECUTTER_TEST_FRAGMENTS
 
 KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
 COOKIECUTTER_SUBMISSIONS_PATH = join('submissions')
-TEST_FRAGMENTS =  KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_SUBMISSIONS_PATH
+TEST_FRAGMENTS = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_SUBMISSIONS_PATH
 
-TRAIN_FRAGMENTS = ['1']
+TRAIN_FRAGMENTS = ['1', '2']
 VAL_FRAGMENTS = ['3']
 
 Z_START = 27
