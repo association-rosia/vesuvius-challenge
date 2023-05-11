a53624389435dc7a72ecc042dd1ca0a3c7be0f0a
Louis REBERGA <louis.reberga@gmail.com>
2023-05-11 08:47:03 +0200

    data augmentation transforms added to dataset class

--- constant.py
+++ constant.py
@@ -4,9 +4,11 @@ from os import pardir
 KAGGLE_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'train')
 COOKIECUTTER_FRAGMENTS_PATH = join('data', 'raw', 'train')
 PARENT_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'train')
+NOTEBOOK_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'train')
 FRAGMENTS_PATH = KAGGLE_FRAGMENTS_PATH if exists(KAGGLE_FRAGMENTS_PATH) \
                                        else COOKIECUTTER_FRAGMENTS_PATH if exists(COOKIECUTTER_FRAGMENTS_PATH) \
-                                       else PARENT_FRAGMENTS_PATH
+                                       else PARENT_FRAGMENTS_PATH if exists(PARENT_FRAGMENTS_PATH) \
+                                       else NOTEBOOK_FRAGMENTS_PATH
 
 KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
 COOKIECUTTER_MODELS_DIR = 'models'
