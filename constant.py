6a5ec41eb7f3e2b3ff9dc2a37676adc4251f9a46
Louis REBERGA <louis.reberga@gmail.com>
2023-05-14 23:36:39 +0200

    del + gc.collect()

--- constant.py
+++ constant.py
@@ -1,29 +1,69 @@
 from os.path import join, exists
 from os import pardir
 
-KAGGLE_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'train')
-COOKIECUTTER_FRAGMENTS_PATH = join('data', 'raw', 'train')
-PARENT_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'train')
-NOTEBOOK_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'train')
-FRAGMENTS_PATH = KAGGLE_FRAGMENTS_PATH if exists(KAGGLE_FRAGMENTS_PATH) \
-                                       else COOKIECUTTER_FRAGMENTS_PATH if exists(COOKIECUTTER_FRAGMENTS_PATH) \
-                                       else PARENT_FRAGMENTS_PATH if exists(PARENT_FRAGMENTS_PATH) \
-                                       else NOTEBOOK_FRAGMENTS_PATH
+###### KAGGLE DIRECTORIES ######
 
 KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
+KAGGLE_TRAIN_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'train')
+KAGGLE_TEST_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'test')
+KAGGLE_SAVE_PATH = join(pardir, pardir, 'working', 'data', 'raw')
+
+################################
+
+### COOKIECUTTER DIRECTORIES ###
+
 COOKIECUTTER_MODELS_DIR = 'models'
+COOKIECUTTER_SUBMISSIONS_DIR = 'submissions'
+COOKIECUTTER_TRAIN_FRAGMENTS_PATH = join('data', 'raw', 'train')
+COOKIECUTTER_TEST_FRAGMENTS_PATH = join('data', 'raw', 'test')
+COOKIECUTTER_TRAIN_SAVE_PATH = join('data', 'processed', 'train')
+COOKIECUTTER_TEST_SAVE_PATH = join('data', 'processed', 'test')
+
+PARENT_TRAIN_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'train')
+PARENT_TEST_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'test')
+PARENT_TRAIN_SAVE_PATH = join(pardir, pardir, 'data', 'processed', 'train')
+PARENT_TEST_SAVE_PATH = join(pardir, pardir, 'data', 'processed', 'test')
+
+################################
+
+#### NOTEBOOKS DIRECTORIES ####
+NOTEBOOK_TRAIN_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'train')
+NOTEBOOK_TEST_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'test')
+
+###############################
+
+######### DIRECTORIES #########
+
 MODELS_DIR = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_MODELS_DIR
+SUBMISSION_DIR = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_SUBMISSIONS_DIR
 
-KAGGLE_TEST_FRAGMENTS = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'test')
-COOKIECUTTER_TEST_FRAGMENTS = join('data', 'raw', 'test')
-TEST_FRAGMENTS = KAGGLE_TEST_FRAGMENTS if exists(KAGGLE_TEST_FRAGMENTS) else COOKIECUTTER_TEST_FRAGMENTS
+TRAIN_FRAGMENTS_PATH = KAGGLE_TRAIN_FRAGMENTS_PATH if exists(KAGGLE_TRAIN_FRAGMENTS_PATH) \
+    else COOKIECUTTER_TRAIN_FRAGMENTS_PATH if exists(COOKIECUTTER_TRAIN_FRAGMENTS_PATH) \
+    else PARENT_TRAIN_FRAGMENTS_PATH if exists(PARENT_TRAIN_FRAGMENTS_PATH) \
+    else NOTEBOOK_TRAIN_FRAGMENTS_PATH
 
-KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
-COOKIECUTTER_SUBMISSIONS_PATH = join('submissions')
-TEST_FRAGMENTS = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_SUBMISSIONS_PATH
+TEST_FRAGMENTS_PATH = KAGGLE_TEST_FRAGMENTS_PATH if exists(KAGGLE_TEST_FRAGMENTS_PATH) \
+    else COOKIECUTTER_TEST_FRAGMENTS_PATH if exists(COOKIECUTTER_TEST_FRAGMENTS_PATH) \
+    else PARENT_TEST_FRAGMENTS_PATH if exists(PARENT_TEST_FRAGMENTS_PATH) \
+    else NOTEBOOK_TEST_FRAGMENTS_PATH
+
+TRAIN_SAVE_PATH = KAGGLE_SAVE_PATH if exists(KAGGLE_WORKING_DIR) \
+    else COOKIECUTTER_TRAIN_SAVE_PATH if exists(COOKIECUTTER_TRAIN_FRAGMENTS_PATH) \
+    else PARENT_TRAIN_SAVE_PATH if exists(PARENT_TRAIN_FRAGMENTS_PATH) \
+    else ''
+
+TEST_SAVE_PATH = KAGGLE_SAVE_PATH if exists(KAGGLE_WORKING_DIR) \
+    else COOKIECUTTER_TEST_SAVE_PATH if exists(COOKIECUTTER_TEST_FRAGMENTS_PATH) \
+    else PARENT_TEST_SAVE_PATH if exists(PARENT_TEST_FRAGMENTS_PATH) \
+    else ''
+
+###############################
+
+########## VARIABLES ##########
 
 TRAIN_FRAGMENTS = ['1', '2']
 VAL_FRAGMENTS = ['3']
+TEST_FRAGMENTS = ['a', 'b', 'c']
 
 Z_START = 27
 Z_DIM = 8
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -15,17 +15,25 @@ from torchvision import transforms as T
 import numpy as np
 from tiler import Tiler
 
+import gc
+
 from src.utils import get_device
-from constant import FRAGMENTS_PATH, TRAIN_FRAGMENTS, Z_START, Z_DIM, TILE_SIZE
+from constant import (TRAIN_FRAGMENTS_PATH, TEST_FRAGMENTS_PATH,
+                      Z_START, Z_DIM, TILE_SIZE,
+                      TRAIN_FRAGMENTS)
 
 DEVICE = get_device()
 
 
-def tile_fragment(fragment):
-    fragment_path = os.path.join(FRAGMENTS_PATH, fragment)
+def tile_fragment(set_path, fragment):
+    fragment_path = os.path.join(set_path, fragment)
     image_path = sorted(glob.glob(os.path.join(fragment_path, 'surface_volume/*.tif')))[Z_START:Z_START + Z_DIM]
-    image = [cv2.imread(slice_path, cv2.IMREAD_GRAYSCALE) / 255.0 for slice_path in image_path]
-    image = np.stack(image, axis=0)
+    stack_list = [cv2.imread(slice_path, cv2.IMREAD_GRAYSCALE) / 255.0 for slice_path in image_path]
+    image = np.stack(stack_list, axis=0)
+
+    del stack_list
+    gc.collect()
+
     image_tiler = Tiler(data_shape=image.shape,
                         tile_shape=(Z_DIM, TILE_SIZE, TILE_SIZE),
                         overlap=0.5,
@@ -47,60 +55,52 @@ def tile_fragment(fragment):
     fragment_list = []
     image_list = []
     mask_list = []
-    tile_bbox_list = []
     tiles_zip = zip(image_tiler(image_pad), mask_tiler(mask_pad))
 
     for image_tile, mask_tile in tiles_zip:
         if mask_tile[1].max() > 0:
-            fragment_list.append(torch.from_numpy(np.asarray([np.float32(fragment)])))
+            fragment_list.append(fragment)
             image_list.append(torch.from_numpy(image_tile[1].astype('float32')))
             mask_list.append(torch.from_numpy(mask_tile[1].astype('float32')))
-            tile_bbox_list.append(
-                torch.from_numpy(np.array(image_tiler.get_tile_bbox(image_tile[0])).astype('float32'))
-            )
 
-    fragment = torch.stack(fragment_list, dim=0)
+    fragment = fragment_list
     image = torch.stack(image_list, dim=0)
     mask = torch.stack(mask_list, dim=0)
-    tile_bbox = torch.stack(tile_bbox_list, dim=0)
 
-    return fragment, image, mask, tile_bbox
+    del fragment_list, image_list, mask_list
+    gc.collect()
+
+    return fragment, image, mask
 
 
 class CustomDataset(Dataset):
-    def __init__(self, fragments, augmentation):
+    def __init__(self, fragments, test, augmentation, multi_context):
         self.image = torch.Tensor()
         self.mask = torch.Tensor()
-        self.tile_bbox = torch.Tensor()
-        self.fragment = torch.Tensor()
+        self.fragment = []
 
+        set_path = TRAIN_FRAGMENTS_PATH if not test else TEST_FRAGMENTS_PATH
+        # save_path = TRAIN_SAVE_PATH if not test else TEST_SAVE_PATH
         for fragment in fragments:
-            fragment, image, mask, tile_bbox = tile_fragment(fragment)
+            fragment, image, mask = tile_fragment(set_path, fragment)
             self.image = torch.cat((self.image, image), dim=0)
             self.mask = torch.cat((self.mask, mask), dim=0)
-            self.tile_bbox = torch.cat((self.tile_bbox, tile_bbox), dim=0)
-            self.fragment = torch.cat((self.fragment, fragment), dim=0)
+            self.fragment += fragment
 
         self.augmentation = augmentation
         self.transforms = T.RandomApply(nn.ModuleList([T.RandomRotation(180),
                                                        T.RandomPerspective(),
                                                        T.ElasticTransform(alpha=500.0, sigma=10.0),
                                                        T.RandomHorizontalFlip(),
-                                                       T.RandomVerticalFlip()
-                                                       ]), p=0.5)
+                                                       T.RandomVerticalFlip()]), p=0.5)
 
     def __len__(self):
-        return len(self.image)
+        return len(self.fragment)
 
     def __getitem__(self, idx):
+        fragment = self.fragment[idx]
         image = torch.unsqueeze(self.image[idx], dim=0).to(DEVICE)
-        mask = torch.unsqueeze(torch.unsqueeze(self.mask[idx], dim=0), dim=0).to(DEVICE)
-
-        bbox = self.tile_bbox[idx]
-        center = [(bbox[0, 0] + bbox[1, 0]) / 2, (bbox[0, 1] + bbox[1, 1]) / 2]
-        center = torch.FloatTensor(center).to(DEVICE)
-
-        fragment = self.fragment[idx].to(DEVICE)  # fragment id for reconstruction -> 1, 2 or 3
+        mask = torch.unsqueeze(self.mask[idx], dim=0).to(DEVICE)  # don't remove it Baptiste
 
         if self.augmentation:
             seed = random.randint(0, 2 ** 32)
@@ -109,13 +109,24 @@ class CustomDataset(Dataset):
             torch.manual_seed(seed)
             mask = self.transforms(mask)
 
-        return fragment, image, mask, center
+        return fragment, image, mask
+
+#
+# def get_mask_sizes(fragments):
+#     mask_sizes = {}
+#     for fragment in fragments:
+#         fragment_path = os.path.join(FRAGMENTS_PATH, fragment)
+#         mask_path = os.path.join(fragment_path, 'inklabels.png')
+#         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+#         mask_sizes[fragment] = mask.shape
+#
+#     return mask_sizes
 
 
 if __name__ == '__main__':
-    train_dataset = CustomDataset(TRAIN_FRAGMENTS, augmentation=True)
+    train_dataset = CustomDataset(TRAIN_FRAGMENTS, test=False, augmentation=True, multi_context=False)
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=16)
 
-    for indexes, inputs, masks, coords in train_dataloader:
-        print(indexes, inputs.shape, masks.shape, coords)
+    for indexes, inputs, masks in train_dataloader:
+        print(indexes, inputs.shape, masks.shape)
         break
