e181a077d09a107b2d6553afd1c077972a271e4c
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-14 22:16:10 +0200

    Bug fix metrics.py and utils.py Reconstruction images work well. F05Score work well.

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ from constant import TILE_SIZE
 
 def reconstruct_images(
     sub_masks: torch.Tensor,
-    center_coords: torch.Tensor,
+    bboxs: torch.Tensor,
     fragment_ids: List,
     mask_sizes: Dict,
 ):
@@ -30,23 +30,26 @@ def reconstruct_images(
     }
 
     for i in range(sub_masks.shape[0]):
-        x_center, y_center = center_coords[i, 0], center_coords[i, 1]
+        # x_center, y_center = center_coords[i, 0], center_coords[i, 1]
 
-        # Calculate the start and end coordinates of the sub-mask
-        x_start = x_center - TILE_SIZE // 2
-        x_end = x_start + TILE_SIZE
-        y_start = y_center - TILE_SIZE // 2
-        y_end = y_start + TILE_SIZE
+        # # Calculate the start and end coordinates of the sub-mask
+        # x_start = x_center - TILE_SIZE // 2
+        # x_end = x_start + TILE_SIZE
+        # y_start = y_center - TILE_SIZE // 2
+        # y_end = y_start + TILE_SIZE
 
         # Handle overlap by taking the mean of overlapping pixels
         reconstructed_images[fragment_ids[i]][
-            y_start:y_end, x_start:x_end
+            bboxs[i, 0, 0] : bboxs[i, 1, 0], bboxs[i, 0, 1] : bboxs[i, 1, 1]
         ] += sub_masks[i, :, :]
-        count_map[fragment_ids[i]][y_start:y_end, x_start:x_end] += 1
-
+        count_map[fragment_ids[i]][
+            bboxs[i, 0, 0] : bboxs[i, 1, 0], bboxs[i, 0, 1] : bboxs[i, 1, 1]
+        ] += 1
+        
     # Divide by the count map to obtain the mean value
     for key in mask_sizes.keys():
         reconstructed_images[key] /= count_map[key]
+        reconstructed_images[key] = torch.nan_to_num(reconstructed_images[key], nan=0)
 
     return reconstructed_images
 
