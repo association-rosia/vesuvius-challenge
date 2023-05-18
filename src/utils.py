fc5e4679b98ab42c3a1e24e6d3c43deb9d7e3110
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-18 16:09:41 +0200

    Move get_image_shape to utils.py and renamed get_mask_shape. Bug fix: Dimension of the mask is not the same if the dataset is augmented or not.

--- src/utils.py
+++ src/utils.py
@@ -7,14 +7,16 @@ from typing import List, Dict
 import torch
 import numpy as np
 
-from constant import TILE_SIZE
+import cv2
+
+from constant import TILE_SIZE, TRAIN_FRAGMENTS_PATH, TEST_FRAGMENTS_PATH
 
 
 def reconstruct_images(
     sub_masks: torch.Tensor,
     bboxs: torch.Tensor,
     fragment_ids: List,
-    mask_sizes: Dict,
+    mask_shapes: Dict,
 ):
     # Implementation of the reconstruction logic
     # Combine sub-masks to reconstruct the original images separately
@@ -22,11 +24,11 @@ def reconstruct_images(
 
     reconstructed_images = {
         fragment_id: torch.zeros(mask_size).to(device=sub_masks.device)
-        for fragment_id, mask_size in mask_sizes.items()
+        for fragment_id, mask_size in mask_shapes.items()
     }
     count_map = {
         fragment_id: torch.zeros(mask_size).to(device=sub_masks.device)
-        for fragment_id, mask_size in mask_sizes.items()
+        for fragment_id, mask_size in mask_shapes.items()
     }
 
     for i in range(sub_masks.shape[0]):
@@ -45,19 +47,33 @@ def reconstruct_images(
         count_map[fragment_ids[i]][
             bboxs[i, 0, 0] : bboxs[i, 1, 0], bboxs[i, 0, 1] : bboxs[i, 1, 1]
         ] += 1
-        
+
     # Divide by the count map to obtain the mean value
-    for key in mask_sizes.keys():
+    for key in mask_shapes.keys():
         reconstructed_images[key] /= count_map[key]
         reconstructed_images[key] = torch.nan_to_num(reconstructed_images[key], nan=0)
 
     return reconstructed_images
 
 
+def get_mask_shape(fragment_dir):
+    mask_path = os.path.join(fragment_dir, "inklabels.png")
+    return cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE).shape
+
+
+def get_dict_mask_shapes(list_fragment_ids, test: bool = False):
+    set_path = TRAIN_FRAGMENTS_PATH if not test else TEST_FRAGMENTS_PATH
+
+    return {
+        fragment_id: get_mask_shape(os.path.join(set_path, fragment_id))
+        for fragment_id in list_fragment_ids
+    }
+
+
 def get_device():
     device = "cpu"
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
+    # if torch.cuda.is_available():
+    #     device = "cuda"
+    # elif torch.backends.mps.is_available():
+    #     device = "mps"
     return torch.device(device=device)
