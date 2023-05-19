462faa2882127910a0ecf2fe94362098b23fa6ff
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-19 22:59:30 +0200

    Change factors alpha beta to bce_weight and  dice_weight.  Add tests.

--- src/models/losses.py
+++ src/models/losses.py
@@ -25,7 +25,7 @@ class CombinedLoss(nn.Module):
         dice_loss = self.dice_loss(inputs, targets)
         
         # Combine the losses using weighted sum
-        combined_loss = self.alpha * bce_loss + self.beta * dice_loss
+        combined_loss = self.bce_weight * bce_loss + self.dice_weight * dice_loss
         
         return combined_loss
     
@@ -43,9 +43,22 @@ class DiceLoss(nn.Module):
     
 
 if __name__ == '__main__':
+    bce_weight = .5
+    
     dice_loss = DiceLoss()
+    bce_loss = nn.BCELoss()
+    combined_loss = CombinedLoss(bce_weight)
     
-    prediction = torch.randint(2, size=(8, 256, 256))
-    label = torch.randint(2, size=(8, 256, 256))
+    prediction = torch.randint(2, size=(8, 256, 256)).to(dtype=torch.float32)
+    label = torch.randint(2, size=(8, 256, 256)).to(dtype=torch.float32)
     
-    print(dice_loss(prediction, label))
+    print()
+    print('Test with two random tensors.')
+    print('BCE Loss:', bce_loss(prediction, label))
+    print('Dice Loss:', dice_loss(prediction, label))
+    print(f'Combined Loss (BCE Weight {bce_weight}):', combined_loss(prediction, label))
+    print()
+    print('Test with same tensor.')
+    print('BCE Loss:', bce_loss(label, label))
+    print('Dice Loss:', dice_loss(label, label))
+    print(f'Combined Loss (BCE Weight {bce_weight}):', combined_loss(label, label))
