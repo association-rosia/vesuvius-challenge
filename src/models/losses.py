2c8ddbbf03360ff6912b6e3954ce9441c7531bd8
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-19 22:36:20 +0200

    Create DiceLoss class.

--- src/models/losses.py
+++ src/models/losses.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 class CombinedLoss(nn.Module):
     def __init__(self, bce_weight=None, dice_weight=None):
@@ -14,22 +15,37 @@ class CombinedLoss(nn.Module):
             self.dice_weight = 1 - bce_weight
             
         self.bce_loss = nn.BCELoss()
-        # TODO: Develop a DiceLoss Module
-        # self.dice_loss = DiceLoss()
+        self.dice_loss = DiceLoss()
         
     def forward(self, inputs, targets):
         # Compute binary cross-entropy (BCE) loss
         bce_loss = self.bce_loss(inputs, targets)
         
         # Compute Dice loss
-        # * Uncomment next line when the DiceLoss are implemented
-        # dice_loss = self.dice_loss(inputs, targets)
+        dice_loss = self.dice_loss(inputs, targets)
         
         # Combine the losses using weighted sum
-        # * Uncomment next line when the DiceLoss are implemented
-        # combined_loss = self.alpha * bce_loss + self.beta * dice_loss
-        
-        # * Delete next line when the DiceLoss are implemented
-        combined_loss = bce_loss
+        combined_loss = self.alpha * bce_loss + self.beta * dice_loss
         
         return combined_loss
+    
+    
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor):
+        intersection = torch.logical_and(y_pred.bool(), y_true.bool()).sum()
+        union = y_pred.sum() + y_true.sum()
+        dice = (2 * intersection.float()) / (union.float() + 1e-7)
+        loss = 1 - dice.mean()
+        return loss
+    
+
+if __name__ == '__main__':
+    dice_loss = DiceLoss()
+    
+    prediction = torch.randint(2, size=(8, 256, 256))
+    label = torch.randint(2, size=(8, 256, 256))
+    
+    print(dice_loss(prediction, label))
