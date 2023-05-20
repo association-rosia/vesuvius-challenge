7b78d445071f3a7d6440e02872416a311832dfaa
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-20 11:38:24 +0200

    Debug and Update make_train.py

--- src/models/make_train.py
+++ src/models/make_train.py
@@ -12,16 +12,18 @@ from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from src.models.losses import CombinedLoss
 from src.models.lightning import LightningVesuvius
 from src.models.unet3d import UNet3d
-from src.data.make_dataset import CustomDataset, get_image_sizes
-from src.utils import get_device
+from src.data.make_dataset import CustomDataset
+from src.utils import get_dict_mask_shapes
 
-from constant import TRAIN_FRAGMENTS, VAL_FRAGMENTS, MODELS_PATH
+from constant import TRAIN_FRAGMENTS, VAL_FRAGMENTS, MODELS_DIR, TILE_SIZE
 
 import wandb
 
+
 def main():
     wandb.init(
-        project='vesuvius-challenge-ink-detection'
+        project='vesuvius-challenge-ink-detection',
+        entity='winged-bull'
     )
     
     # empty the GPU cache
@@ -30,14 +32,14 @@ def main():
     model = get_model()
     
     train_dataloader = DataLoader(
-        dataset=CustomDataset(TRAIN_FRAGMENTS),
+        dataset=CustomDataset(TRAIN_FRAGMENTS, augmentation=True, test=False),
         batch_size=wandb.config.batch_size,
         shuffle=True,
         drop_last=True
         )
     
     val_dataloader = DataLoader(
-        dataset=CustomDataset(VAL_FRAGMENTS),
+        dataset=CustomDataset(VAL_FRAGMENTS, augmentation=False, test=False),
         batch_size=wandb.config.batch_size,
         shuffle=False,
         drop_last=True
@@ -53,31 +55,38 @@ def main():
 
 
 def get_model():
-    if wandb.config.model == 'UNet3d':
+    if wandb.config.model_name == 'UNet3d':
         num_block = wandb.config.num_block
-        list_channels = [32 * i**2 for i in range(0, num_block + 1)]
-        pytorch_model = UNet3d(list_channels)
+        model_parameters = dict(
+            list_channels=[1] + [32 * 2**(i) for i in range(num_block)],
+            inputs_size=TILE_SIZE,
+        )
+        
+        # list_channels = [1] + [32 * 2**(i) for i in range(num_block)]
+        # pytorch_model = UNet3d(list_channels, inputs_size=TILE_SIZE)
     
     # get the device
-    device = get_device()
-    pytorch_model#.to(device)
+    # device = get_device()
+    # pytorch_model#.to(device)
     
-    learning_rate = wandb.config.learning_rate
+    # learning_rate = wandb.config.learning_rate
     
-    dice_weight = wandb.config.dice_weight
-    criterion = CombinedLoss(dice_weight=dice_weight)
+    # bce_weight = wandb.config.bce_weight
+    # criterion = CombinedLoss(bce_weight=bce_weight)
     
-    scheduler_patience = wandb.config.scheduler_patience
+    # scheduler_patience = wandb.config.scheduler_patience
 
-    val_image_sizes = get_image_sizes(VAL_FRAGMENTS)
+    val_mask_shapes = get_dict_mask_shapes(VAL_FRAGMENTS)
     
     lightning_model = LightningVesuvius(
-        pytorch_model, 
-        learning_rate,
-        scheduler_patience,
-        criterion, 
-        val_image_sizes,
-        ).to(device)
+        model_name=wandb.config.model_name,
+        model_parameters=model_parameters,
+        learning_rate=wandb.config.learning_rate,
+        scheduler_patience=wandb.config.scheduler_patience,
+        bce_weight=wandb.config.bce_weight,
+        f05score_threshold=wandb.config.f05score_threshold, 
+        val_mask_shapes=get_dict_mask_shapes(VAL_FRAGMENTS),
+        )#.to(device)
     
     return lightning_model
 
@@ -86,10 +95,10 @@ def get_trainer():
     
     checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
-        monitor="val_F05Score",
+        monitor="val/F05Score",
         mode="max",
-        dirpath=MODELS_PATH,
-        filename="{val_F05Score}-{wandb.name}-{wandb.id}"
+        dirpath=MODELS_DIR,
+        filename="{val/F05Score:.5f}-" + f"{wandb.run.name}-{wandb.run.id}"
     )
     
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
@@ -105,4 +114,20 @@ def get_trainer():
 
 
 if __name__ == '__main__':
+    wandb.init(
+        project='vesuvius-challenge-ink-detection',
+        entity='winged-bull',
+        group='test',
+        config=dict(
+            batch_size=16,
+            model_name='UNet3d',
+            num_block=2,
+            bce_weight=1,
+            scheduler_patience=3,
+            learning_rate=0.0001,
+            epochs=3,
+            f05score_threshold=.5
+        )
+    )
+    
     main()
