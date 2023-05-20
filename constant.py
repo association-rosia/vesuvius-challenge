9ea016bf15e52ab9e5ebc73f5047607bed4ecc34
Moncoutiej <johan6446@gmail.com>
2023-05-20 19:31:55 -0400

    Rearrange the Kaggle directories in absolute paths

--- constant.py
+++ constant.py
@@ -2,10 +2,10 @@ from os.path import join, exists
 from os import pardir
 
 # KAGGLE DIRECTORIES
-KAGGLE_WORKING_DIR = join(pardir, pardir, 'working')
-KAGGLE_TRAIN_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'train')
-KAGGLE_TEST_FRAGMENTS_PATH = join(pardir, pardir, 'input', 'vesuvius-challenge-ink-detection', 'test')
-KAGGLE_SAVE_PATH = join(pardir, pardir, 'working', 'data', 'raw')
+KAGGLE_WORKING_DIR = '/kaggle/input/'
+KAGGLE_TRAIN_FRAGMENTS_PATH = '/kaggle/input/vesuvius-challenge-ink-detection/train/'
+KAGGLE_TEST_FRAGMENTS_PATH = '/kaggle/input/vesuvius-challenge-ink-detection/test/'
+KAGGLE_SAVE_PATH = '/kaggle/working/data/raw'
 
 # COOKIECUTTER DIRECTORIES
 COOKIECUTTER_MODELS_DIR = 'models'
