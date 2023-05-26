ffbed292df614595425ccf9a0a42696d45a125cd
Louis REBERGA <louis.reberga@gmail.com>
2023-05-26 22:56:15 +0200

    model to .to(torch.HalfTensor)

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -31,5 +31,5 @@ for fragment, bbox, mask, image in train_dataloader:
     print(image.shape)
     break
 
-model = ResidualUNet3D(in_channels=1, out_channels=1).to(device)
+model = ResidualUNet3D(in_channels=1, out_channels=1).to(torch.HalfTensor)
 res = model(image)
