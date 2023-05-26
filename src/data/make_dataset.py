d4560e5b8486ddd62f0bec4f764416e1e0652473
Louis REBERGA <louis.reberga@gmail.com>
2023-05-26 22:42:38 +0200

    tensor make dataset .to(device)

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -129,8 +129,8 @@ class DatasetVesuvius(Dataset):
             torch.manual_seed(seed)
             mask = torch.squeeze(self.transforms(mask))
 
-        image = image.type(torch.HalfTensor)
-        mask = mask.type(torch.HalfTensor)
+        image = image.type(torch.HalfTensor).to(device)
+        mask = mask.type(torch.HalfTensor).to(device)
 
         return fragment, bbox, mask, image
 
