f30f05fef51b7d75e7dc1ad69df355cac4a90936
Louis REBERGA <louis.reberga@gmail.com>
2023-05-26 23:02:39 +0200

    model to .half().to(device)

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -31,5 +31,5 @@ for fragment, bbox, mask, image in train_dataloader:
     print(image.shape)
     break
 
-model = ResidualUNet3D(in_channels=1, out_channels=1).to(torch.float16)
+model = ResidualUNet3D(in_channels=1, out_channels=1).half().to(device)
 res = model(image)
