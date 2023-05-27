e6ac19ba790d259dfc88c0461351780850e09635
Louis REBERGA <louis.reberga@gmail.com>
2023-05-27 23:23:46 +0200

    x0, y0, x1, y1 = bboxes[i]

--- src/utils.py
+++ src/utils.py
@@ -24,22 +24,15 @@ def reconstruct_images(sub_masks: torch.Tensor, bboxes: torch.Tensor, fragments:
     }
 
     for i in range(sub_masks.shape[0]):
-        reconstructed_images[fragments[i]][
-            bboxes[i, 0]: bboxes[i, 2], bboxes[i, 1]: bboxes[i, 3]
-        ] += sub_masks[i, :, :]
-
-        count_map[fragments[i]][
-            bboxes[i, 0]: bboxes[i, 2], bboxes[i, 1]: bboxes[i, 3]
-        ] += 1
+        x0, y0, x1, y1 = bboxes[i]
+        print(x0, y0, x1, y1)
+        reconstructed_images[fragments[i]][x0:x1, y0:y1] += sub_masks[i, :, :]
+        count_map[fragments[i]][x0:x1, y0:y1] += 1
 
     # Divide by the count map to obtain the mean value
     for key in mask_shapes.keys():
         reconstructed_images[key] /= count_map[key]
         reconstructed_images[key] = torch.nan_to_num(reconstructed_images[key], nan=0)
-
-    print('--------------------------------')
-    print(reconstructed_images)
-    print('--------------------------------')
 
     return reconstructed_images
 
