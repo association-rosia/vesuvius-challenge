9f1d4c2f2b9ab40a8e25f49d84391e9f10f22024
Louis REBERGA <louis.reberga@gmail.com>
2023-05-29 19:51:05 +0200

    accelerator='cpu'

--- src/models/make_train.py
+++ src/models/make_train.py
@@ -96,8 +96,8 @@ def get_trainer():
 
     # init the trainer
     trainer = pl.Trainer(
-        accelerator='gpu',
-        devices=1,
+        accelerator='cpu',
+        # devices=1,
         max_epochs=wandb.config.epochs,
         callbacks=[lr_monitor, checkpoint_callback],
         logger=WandbLogger(),
