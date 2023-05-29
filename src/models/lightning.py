72873e12cb48d1f11b2e403e4b0f5674fe943819
Louis REBERGA <louis.reberga@gmail.com>
2023-05-29 19:48:02 +0200

    print steps

--- src/models/lightning.py
+++ src/models/lightning.py
@@ -34,12 +34,15 @@ class LightningVesuvius(pl.LightningModule):
         return x
 
     def training_step(self, batch, batch_idx):
-        print(next(self.pytorch_model.parameters()).is_cuda)
         _, _, masks, images = batch
+        print('_, _, masks, images = batch')
         outputs = self.forward(images)
+        print('outputs = self.forward(images)')
 
         loss = self.criterion(outputs, masks)
+        print('loss = self.criterion(outputs, masks)')
         self.log('train/loss', loss, on_step=False, on_epoch=True)
+        print('self.log(train/loss, loss, on_step=False, on_epoch=True)')
 
         return {'loss': loss}
 
