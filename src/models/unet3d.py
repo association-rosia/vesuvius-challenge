ccdc7f41b369e705d28de78384173abeadf45a71
BaptisteUrgell <baptiste.u@gmail.com>
2023-05-29 15:34:54 +0200

    Add tensor to cuda

--- src/models/unet3d.py
+++ src/models/unet3d.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
 
     from constant import TILE_SIZE
 
-    model = Unet3d(nb_blocks=3, inputs_size=TILE_SIZE).to(device='gpu')
+    model = Unet3d(nb_blocks=3, inputs_size=TILE_SIZE).to(device='cuda')
     print(model)
-    inputs = torch.randn((8, 1, 8, 256, 256))
+    inputs = torch.randn((8, 1, 8, 256, 256)).to(device='cuda')
     print(model(inputs))
