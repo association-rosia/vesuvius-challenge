9b57b14ac7e3951cc30dc2b95e6914c151d38329
Louis REBERGA <louis.reberga@gmail.com>
2023-05-31 12:23:51 +0200

    Revert "remove batch to device because this is a list"
    This reverts commit b7b2ee526b7de85b407837b1b67561269d1728b4.

--- src/models/pytorch.py
+++ src/models/pytorch.py
@@ -64,7 +64,7 @@ metric = F05Score(get_fragments_shape(VAL_FRAGMENTS, TILE_SIZE)).to(DEVICE)
 
 training_loss = MeanMetric().to(DEVICE)
 for i, batch in tqdm(enumerate(training_loader), total=len(training_loader)):
-    _, _, masks, images = batch
+    _, _, masks, images = batch.to(DEVICE)
     masks = masks.to(DEVICE)
     images = images.to(DEVICE)
 
