5007ab2c9f1593377dd77ce3a10255375b88fbf9
Moncoutiej <johan6446@gmail.com>
2023-06-04 11:32:58 -0400

    Add TTA Vertical and Horizontal flip

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -259,3 +259,57 @@ class TTAElasticTransform(DualTransform):
 
     def apply_deaug_label(self, label, **kwargs):
         return label
+
+
+class TTAHorizontalFlip(DualTransform):
+    """Flip images horizontally (left->right)"""
+
+    identity_param = False
+
+    def __init__(self):
+        super().__init__("apply", [False, True])
+
+    def apply_aug_image(self, image, apply=False, **kwargs):
+        if apply:
+            image = F.hflip(image)
+        return image
+
+    def apply_deaug_mask(self, mask, apply=False, **kwargs):
+        if apply:
+            mask = F.hflip(mask)
+        return mask
+
+    def apply_deaug_label(self, label, apply=False, **kwargs):
+        return label
+
+    def apply_deaug_keypoints(self, keypoints, apply=False, **kwargs):
+        if apply:
+            keypoints = F.keypoints_hflip(keypoints)
+        return keypoints
+
+
+class TTAVerticalFlip(DualTransform):
+    """Flip images vertically (up->down)"""
+
+    identity_param = False
+
+    def __init__(self):
+        super().__init__("apply", [False, True])
+
+    def apply_aug_image(self, image, apply=False, **kwargs):
+        if apply:
+            image = F.vflip(image)
+        return image
+
+    def apply_deaug_mask(self, mask, apply=False, **kwargs):
+        if apply:
+            mask = F.vflip(mask)
+        return mask
+
+    def apply_deaug_label(self, label, apply=False, **kwargs):
+        return label
+
+    def apply_deaug_keypoints(self, keypoints, apply=False, **kwargs):
+        if apply:
+            keypoints = F.keypoints_vflip(keypoints)
+        return keypoints
