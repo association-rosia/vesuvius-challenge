52e991d496041f3ab745464541638d4d8915c9cf
Louis REBERGA <louis.reberga@gmail.com>
2023-06-04 11:41:24 +0200

    resolve issue

--- src/constant.py
+++ src/constant.py
@@ -9,6 +9,14 @@ VAL_FRAGMENTS = ['3']
 Z_DIM = 8
 TILE_SIZE = 256
 
+BATCH_SIZE = 4
+NB_BLOCKS = 3
+BCE_WEIGHT = 0.5
+DICE_THRESHOLD = 0.5
+SCHEDULER_PATIENCE = 5
+LR = 1e-5
+EPOCHS = 20
+
 # KAGGLE DIRECTORIES
 KAGGLE_WORKING_DIR = join(abspath(sep), 'kaggle', 'working')
 KAGGLE_INPUT_DIR = join(abspath(sep), 'kaggle', 'input')
@@ -20,15 +28,15 @@ KAGGLE_TILES_PATH = join(KAGGLE_INPUT_DIR, 'vesuvius-challenge-256x256-tiles', '
 # COOKIECUTTER DIRECTORIES
 COOKIECUTTER_MODELS_DIR = 'models'
 COOKIECUTTER_SUBMISSIONS_DIR = 'submissions'
-COOKIECUTTER_TRAIN_FRAGMENTS_PATH = join('../data', 'raw', 'train')
-COOKIECUTTER_TEST_FRAGMENTS_PATH = join('../data', 'raw', 'test')
+COOKIECUTTER_TRAIN_FRAGMENTS_PATH = join('data', 'raw', 'train')
+COOKIECUTTER_TEST_FRAGMENTS_PATH = join('data', 'raw', 'test')
 
-PARENT_TRAIN_FRAGMENTS_PATH = join(pardir, pardir, '../data', 'raw', 'train')
-PARENT_TEST_FRAGMENTS_PATH = join(pardir, pardir, '../data', 'raw', 'test')
+PARENT_TRAIN_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'train')
+PARENT_TEST_FRAGMENTS_PATH = join(pardir, pardir, 'data', 'raw', 'test')
 
 # NOTEBOOKS DIRECTORIES
-NOTEBOOK_TRAIN_FRAGMENTS_PATH = join(pardir, '../data', 'raw', 'train')
-NOTEBOOK_TEST_FRAGMENTS_PATH = join(pardir, '../data', 'raw', 'test')
+NOTEBOOK_TRAIN_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'train')
+NOTEBOOK_TEST_FRAGMENTS_PATH = join(pardir, 'data', 'raw', 'test')
 
 # DIRECTORIES
 MODELS_DIR = KAGGLE_WORKING_DIR if exists(KAGGLE_WORKING_DIR) else COOKIECUTTER_MODELS_DIR
--- src/models/make_train.py
+++ src/models/make_train.py
@@ -12,7 +12,7 @@ from src.data.make_dataset import DatasetVesuvius
 from src.models.lightning import LightningVesuvius
 from src.utils import get_fragments_shape, get_device
 
-from src.constant import TRAIN_FRAGMENTS, VAL_FRAGMENTS, MODELS_DIR, TILE_SIZE, Z_DIM
+import src.constant as cst
 
 import wandb
 
@@ -92,7 +92,7 @@ def get_trainer():
         save_top_k=1,
         monitor='val/loss',
         mode='min',
-        dirpath=MODELS_DIR,
+        dirpath=cst.MODELS_DIR,
         filename=f'{wandb.run.name}-{wandb.run.id}',
     )
 
@@ -117,18 +117,18 @@ if __name__ == '__main__':
             entity='rosia-lab',
             group='test',
             config={
-                'batch_size': 5,
+                'batch_size': cst.BATCH_SIZE,
                 'model_name': 'UNet3D',
-                'nb_blocks': 3,
-                'bce_weight': 0.5,
-                'dice_threshold': 0.5,
-                'scheduler_patience': 5,
-                'learning_rate': 0.00001,
-                'epochs': 20,
-                'tile_size': TILE_SIZE,
-                'num_slices': Z_DIM,
-                'train_fragments': TRAIN_FRAGMENTS,
-                'val_fragments': VAL_FRAGMENTS,
+                'nb_blocks': cst.NB_BLOCKS,
+                'bce_weight': cst.BCE_WEIGHT,
+                'dice_threshold': cst.DICE_THRESHOLD,
+                'scheduler_patience': cst.SCHEDULER_PATIENCE,
+                'learning_rate': cst.LR,
+                'epochs': cst.EPOCHS,
+                'tile_size': cst.TILE_SIZE,
+                'num_slices': cst.Z_DIM,
+                'train_fragments': cst.TRAIN_FRAGMENTS,
+                'val_fragments': cst.VAL_FRAGMENTS,
             },
         )
     else:
